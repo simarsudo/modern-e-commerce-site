ad299596d32382acf2189eff1118d8475d678edf
Simarjeet Singh <sjthecoder@gmail.com>
2023-03-20 11:38:21 +0530

    changed text to white

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,12 +5,12 @@ type Props = {};
 const Footer = (props: Props) => {
 	return (
 		<div className="bg-gray-400 px-4 py-2 text-white md:sticky md:p-8">
-			<div className="flex h-full justify-around ">
+			<div className="flex h-full justify-around md:justify-evenly lg:justify-around">
 				<div>
 					<h1 className="text-xl font-bold text-violet-500 underline decoration-violet-500">
 						Shop for
 					</h1>
-					<div className="pl-2 font-semibold text-neutral-200">
+					<div className="pl-2 font-semibold text-white">
 						<h4>Shirt</h4>
 						<h4>TShirt</h4>
 						<h4>Pants</h4>
@@ -22,10 +22,10 @@ const Footer = (props: Props) => {
 					<h1 className="text-xl font-bold text-violet-500 underline decoration-violet-500">
 						Need Help
 					</h1>
-					<div className="pl-2 font-semibold text-neutral-200">
+					<div className="pl-2 font-semibold text-white">
 						<h4>Contact Us</h4>
 						<h4>Track Order</h4>
-						<h4>Returns & Refunds</h4>
+						<h4>Returns/Refunds</h4>
 						<h4>FAQs</h4>
 					</div>
 				</div>
@@ -33,7 +33,7 @@ const Footer = (props: Props) => {
 					<h1 className="text-xl font-bold text-violet-500 underline decoration-violet-500">
 						Company
 					</h1>
-					<div className="pl-2 font-semibold text-neutral-200">
+					<div className="pl-2 font-semibold text-white">
 						<h4>About Us</h4>
 						<h4>Careers</h4>
 					</div>
