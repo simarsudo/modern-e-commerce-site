3302fca05b81a8499c585e9ed6586233e5d517aa
Simarjeet Singh <sjthecoder@gmail.com>
2023-03-21 16:33:49 +0530

    fake data fetching

--- src/typeModels/models.tsx
+++ src/typeModels/models.tsx
@@ -6,4 +6,12 @@ export type product = {
 	imgLink: string;
 };
 
+export type productAPI = {
+	id: string;
+	title: string;
+	category: string;
+	price: number;
+	thumbnail: string;
+};
+
 export type products = product[];
