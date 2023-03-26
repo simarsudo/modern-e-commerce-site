861ba10c8d03a6dd52e8c10777d14cf60ba73967
Simarjeet Singh <sjthecoder@gmail.com>
2023-03-26 21:13:19 +0530

    added first section

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,8 +4,24 @@ type Props = {};
 
 const LandingPage = (props: Props) => {
 	return (
-		<div className="content-wrapper relative flex h-full flex-col gap-4 border-t md:flex-row md:p-2 2xl:p-4">
-			LandingPage
+		<div className="content-wrapper relative flex h-full flex-col gap-4 border-t md:p-2 2xl:p-4">
+			<div className="flex flex-col gap-4 px-2 py-4">
+				<h1 className="border-b pb-4 text-6xl">Shoes</h1>
+				<div className="flex  hover:cursor-grab">
+					<ul className="no-scrollbar flex w-full gap-4 overflow-x-scroll pt-4 md:gap-12">
+						<li className="card">item 1</li>
+						<li className="card">item 2</li>
+						<li className="card">item 3</li>
+						<li className="card">item 4</li>
+						<li className="card">item 5</li>
+						<li className="card">item 3</li>
+						<li className="card">item 4</li>
+						<li className="card">item 5</li>
+						<li className="card">item 4</li>
+						<li className="card">item 5</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
