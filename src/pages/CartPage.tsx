d236d7f647e750afe4705d2860e327cb4b19a346
Simarjeet Singh <sjthecoder@gmail.com>
2023-04-01 20:41:19 +0530

    Fix: Fixed page transition animation delay

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import PageTransitionWrapper from "../wrappers/PageTransitionWrapper";
 
 type Props = {};
 
 const CartPage = (props: Props) => {
-	return <div className="content-wrapper">CartPage</div>;
+	return (
+		<PageTransitionWrapper className="content-wrapper">
+			CartPage
+		</PageTransitionWrapper>
+	);
 };
 
 export default CartPage;
