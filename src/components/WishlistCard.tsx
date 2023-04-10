7586674cd5dde069a00fda42474d62c97b2985eb
Simarjeet Singh <sjthecoder@gmail.com>
2023-04-10 14:20:02 +0530

    Update: Redesign card look

--- src/components/WishlistCard.tsx
+++ src/components/WishlistCard.tsx
@@ -5,28 +5,28 @@ type Props = {};
 
 const WishlistCard = (props: Props) => {
 	return (
-		<div className="col-span-1 aspect-[1/1.3] rounded border-2 bg-bg-dark p-1 text-white shadow-lg">
-			<div className="">
+		<div className="col-span-1 overflow-hidden rounded-xl bg-bg-dark text-white shadow-lg">
+			<div className="overflow-hidden">
 				<img
-					className="w-full object-cover"
-					src="https://picsum.photos/300/400"
+					className="object-cover"
+					src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1680943740_3715930.jpg?format=webp&w=300&dpr=2"
 					alt=""
 				/>
 			</div>
 			<div className="flex flex-col justify-evenly gap-2 p-2">
 				<div className="flex items-center justify-between font-bold">
 					<h3 className="one-line w-2/3 overflow-hidden text-ellipsis px-1 text-lg font-bold">
-						Product Name dawd adw awd adw awd ad dww
+						Louis Vitton da Kacha
 					</h3>
 					<h4 className="w-1/3 min-w-min text-ellipsis whitespace-nowrap px-1 text-end text-base font-light">
-						Type
+						Kacha
 					</h4>
 				</div>
 				<div className="flex items-center gap-2">
-					<button className="filter-btn flex items-center justify-center bg-teal-500">
+					<button className="filter-btn flex items-center justify-center rounded-md bg-teal-500 hover:-translate-y-1 hover:bg-teal-400">
 						<ShoppingCartIcon className="h-5 w-5" />
 					</button>
-					<button className="filter-btn flex items-center justify-center bg-rose-500 hover:bg-rose-600">
+					<button className="filter-btn flex items-center justify-center rounded-md bg-rose-600 hover:-translate-y-1 hover:bg-rose-500">
 						<TrashIcon className="h-5 w-5" />
 					</button>
 				</div>
