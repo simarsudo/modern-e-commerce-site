4e8edb63938ee9798f77f8aafa17293f793dbf98
Simarjeet Singh <sjthecoder@gmail.com>
2023-04-26 18:03:07 +0530

    Add: store added

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	// <React.StrictMode>
