365e0f25cd4be929897be1391442b052843b7472
Simarjeet <sjthecoder@gmail.com>
2023-05-02 21:07:21 +0530

    Add: User login navigation implemented

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,48 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import PageTransitionWrapper from "../wrappers/PageTransitionWrapper";
 import LoginSection from "../sections/LoginSection";
 import SignupSection from "../sections/SignupComponent";
 import ForgotPasswordSection from "../sections/ForgotPasswordSection";
 import SVGBackground from "../components/SVGBackground";
-import Shirt from "../svgImages/Shirt";
-import Pant from "../svgImages/Pant";
-import Shoe from "../svgImages/Shoe";
+import { useAppSelector } from "../store/hooks";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const LoginPage = () => {
-	const [currentComponent, setCurrentComponent] = useState("login");
+    const [currentComponent, setCurrentComponent] = useState("login");
+    const navigate = useNavigate();
+    const location = useLocation();
+    const isAuthenticated = useAppSelector(
+        (state) => state.user.isAuthenticated
+    );
 
-	let Component = (
-		<LoginSection
-			key="loginComponent"
-			setCurrentComponent={setCurrentComponent}
-		/>
-	);
+    useEffect(() => {
+        if (isAuthenticated) {
+            if (location.key !== "default") {
+                navigate(-1);
+            } else {
+                navigate("/");
+            }
+        }
+    }, [isAuthenticated]);
 
-	if (currentComponent === "signup") {
-		Component = (
-			<SignupSection
-				key="signupComponent"
-				setCurrentComponent={setCurrentComponent}
-			/>
-		);
-	} else if (currentComponent === "forgot") {
-		Component = (
-			<ForgotPasswordSection
-				key="forgot"
-				setCurrentComponent={setCurrentComponent}
-			/>
-		);
-	}
+    let Component = (
+        <LoginSection
+            key="loginComponent"
+            setCurrentComponent={setCurrentComponent}
+        />
+    );
 
-	return (
-		<PageTransitionWrapper className="content-wrapper relative flex items-center justify-center">
-			<AnimatePresence mode="wait" initial={false}>
-				{Component}
-			</AnimatePresence>
-			<SVGBackground />
-		</PageTransitionWrapper>
-	);
+    if (currentComponent === "signup") {
+        Component = (
+            <SignupSection
+                key="signupComponent"
+                setCurrentComponent={setCurrentComponent}
+            />
+        );
+    } else if (currentComponent === "forgot") {
+        Component = (
+            <ForgotPasswordSection
+                key="forgot"
+                setCurrentComponent={setCurrentComponent}
+            />
+        );
+    }
+
+    return (
+        <PageTransitionWrapper className="content-wrapper relative flex items-center justify-center">
+            <AnimatePresence mode="wait" initial={false}>
+                {Component}
+            </AnimatePresence>
+            <SVGBackground />
+        </PageTransitionWrapper>
+    );
 };
 
 export default LoginPage;
