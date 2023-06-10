1bb2423198847f66f635a26d346685129838cc71
Simarjeet <sjthecoder@gmail.com>
2023-06-10 16:22:30 +0530

    Add: Swiper image preview and removed old one

--- src/components/ImagesComponent.tsx
+++ src/components/ImagesComponent.tsx
@@ -1,38 +1,50 @@
 import React, { useState } from "react";
+import { register } from "swiper/element/bundle";
+import { SwiperContainer, SwiperSlide } from "swiper/element/bundle";
+import { SwiperProps, SwiperSlideProps } from "swiper/react";
+
+register();
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            "swiper-container": React.DetailedHTMLProps<
+                React.HTMLAttributes<HTMLElement> & SwiperProps,
+                HTMLElement
+            >;
+            "swiper-slide": React.DetailedHTMLProps<
+                React.HTMLAttributes<HTMLElement> & SwiperSlideProps,
+                HTMLElement
+            >;
+        }
+    }
+}
+
+interface Container extends SwiperContainer {}
 
 type Props = {
-	imgs: string[];
+    imgs: string[];
 };
 
 const ImagesComponent = (props: Props) => {
-	const [currentImg, setCurrentImg] = useState(props?.imgs[0]);
-
-	return (
-		<div className="flex min-h-full w-full flex-col-reverse gap-4 overflow-hidden py-4 md:h-[88vh] md:flex-row">
-			<div
-				className={`w-full shrink-0 overflow-hidden overflow-x-auto transition-all md:w-auto md:overflow-y-auto`}
-			>
-				<div className="flex w-full gap-1 overflow-x-auto md:flex-col md:overflow-y-auto">
-					{props.imgs.map((imgLink, key) => {
-						return (
-							<div
-								key={key}
-								className="min-w-[5rem] max-w-[5rem] hover:cursor-pointer md:w-36 lg:min-w-full lg:max-w-max"
-								onClick={() => {
-									setCurrentImg(imgLink);
-								}}
-							>
-								<img src={imgLink} alt="product image" />
-							</div>
-						);
-					})}
-				</div>
-			</div>
-			<div className="flex w-full shrink-0">
-				<img src={currentImg} className="" alt="" />
-			</div>
-		</div>
-	);
+    return (
+        <div className="flex min-h-full">
+            <swiper-container navigation={true} pagination={true}>
+                {props.imgs.map((imgLink, key) => {
+                    return (
+                        <swiper-slide className="flex items-center justify-center">
+                            <img
+                                key={key}
+                                src={imgLink}
+                                alt="product image"
+                                className="mx-auto h-[50vh] w-auto md:h-[80vh]"
+                            />
+                        </swiper-slide>
+                    );
+                })}
+            </swiper-container>
+        </div>
+    );
 };
 
 export default ImagesComponent;
